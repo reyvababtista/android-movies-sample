6850c1253c719d5c8586994402d812f7037a7939
Reyva Babtista <reyvababtista16@gmail.com>
2021-05-27 22:52:06 +0700

    refactor(movie-repository): implement function

--- app/src/main/kotlin/com/rey/movies/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/kotlin/com/rey/movies/data/repository/MovieRepositoryImpl.kt
@@ -6,7 +6,5 @@ import com.rey.movies.domain.dto.MovieResponse
 import com.rey.movies.domain.usecase.repository.MovieRepository
 
 internal class MovieRepositoryImpl(private val remoteData: MovieRemoteData) : MovieRepository {
-    override suspend fun getMovies(): Result<List<MovieResponse>> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getMovies(): Result<List<MovieResponse>> = remoteData.getMovies()
 }
--- app/src/testCommon/kotlin/com/rey/movies/data/source/remote/FakeMovieRemoteDataImpl.kt
+++ app/src/testCommon/kotlin/com/rey/movies/data/source/remote/FakeMovieRemoteDataImpl.kt
@@ -2,6 +2,7 @@ package com.rey.movies.data.source.remote
 
 import com.rey.lib.cleanarch.domain.dto.Result
 import com.rey.lib.cleanarch.domain.dto.UNKNOWN_ERROR
+import com.rey.lib.cleanarch.domain.dto.suspendTryCatch
 import com.rey.movies.data.repository.source.remote.MovieRemoteData
 import com.rey.movies.domain.dto.MovieResponse
 
@@ -9,8 +10,8 @@ class FakeMovieRemoteDataImpl(
     val throwError: Boolean = false,
     val movieList: List<MovieResponse> = emptyList()
 ) : MovieRemoteData {
-    override suspend fun getMovies(): Result<List<MovieResponse>> {
+    override suspend fun getMovies(): Result<List<MovieResponse>> = suspendTryCatch {
         if (throwError) throw Exception(UNKNOWN_ERROR)
-        return Result.Success(movieList)
+        Result.Success(movieList)
     }
 }
