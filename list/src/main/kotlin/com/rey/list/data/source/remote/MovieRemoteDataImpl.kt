5f3c3c475106c987798cf64b0ef14c33c0f36333
Reyva Babtista <reyvababtista16@gmail.com>
2021-05-28 14:52:15 +0700

    refactor(movie-remote-data): optimize imports

--- list/src/main/kotlin/com/rey/list/data/source/remote/MovieRemoteDataImpl.kt
+++ list/src/main/kotlin/com/rey/list/data/source/remote/MovieRemoteDataImpl.kt
@@ -4,12 +4,13 @@ import com.rey.lib.cleanarch.data.source.remote.mapper.ResponseMapper
 import com.rey.lib.cleanarch.domain.dto.Result
 import com.rey.lib.cleanarch.domain.dto.suspendTryCatch
 import com.rey.list.data.repository.source.remote.MovieRemoteData
+import com.rey.list.data.source.remote.api.MovieService
 import com.rey.list.data.source.remote.api.dto.MovieResponseDTO
 import com.rey.movies.domain.dto.MovieResponse
 import javax.inject.Inject
 
 internal class MovieRemoteDataImpl @Inject constructor(
-    private val service: com.rey.list.data.source.remote.api.MovieService,
+    private val service: MovieService,
     private val moviesRespMapper: ResponseMapper<List<MovieResponseDTO>, List<MovieResponse>>
 ) : MovieRemoteData {
     override suspend fun getMovies(): Result<List<MovieResponse>> = suspendTryCatch {
